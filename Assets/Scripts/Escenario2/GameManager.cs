438951b5da83dc56b5f8a779ef6ef0a5d323ffa6
Fede Vico <ifva94@gmail.com>
2020-06-03 15:31:40 +0200

    Finalize Project

--- Assets/Scripts/Escenario2/GameManager.cs
+++ Assets/Scripts/Escenario2/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public GameObject gameOver;
     public GameObject winner;
+    public GameObject menu;
 
 
     private void Start()
@@ -14,17 +15,22 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         gameOver.SetActive(false);
         winner.SetActive(false);
+        menu.SetActive(false);
     }
 
     public void GameOver()
     {
+        Cursor.lockState = CursorLockMode.None;
         gameOver.SetActive(true);
         Time.timeScale = 0;
+        menu.SetActive(true);
     }
 
     public void Winner()
     {
         winner.SetActive(true);
-       
+        menu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+
     }
 }
